c253e828fce07d9618e2a9df7921678376f5f330
tejalkhairnar <khairnarta19.comp@coep.ac.in>
2021-08-25 21:04:00 +0400

    update : added statistics for worldwide data

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "../App.css";
+import { Button } from "reactstrap";
+
+export class Header extends Component {
+  state = {
+    total: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://corona.lmao.ninja/v2/all?yesterday=false")
+      .then((res) => this.setState({ total: res.data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-1 static-top shadow justify-content-between">
+          <a class="nav-item nav-link">
+            ACTIVE CASES: {this.state.total.active}
+          </a>
+          <a class="nav-item nav-link">
+            {" "}
+            RECOVERED CASES:
+            {this.state.total.recovered}
+          </a>
+          <a class="nav-item nav-link">DEATHS: {this.state.total.deaths}</a>
+          <div align="right">
+            <form class="form-inline my-2 my-lg-0">
+              <Button color="danger">
+                <a
+                  href="https://www.who.int/health-topics/coronavirus#tab=tab_1"
+                  className="link"
+                >
+                  <i class="fas fa-virus"></i> About Covid-19{" "}
+                </a>
+              </Button>
+            </form>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
